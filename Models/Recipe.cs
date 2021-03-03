c9aa8e0bccc2dbf7b4072ad3df61be887f5a28b0
Isaac Heist <isaacheist@hotmail.com>
2021-03-03 06:27:13 -0800

    Added Recipe and User models and added sqlite-net-pcl nuget package for managing SQLite DB.

---
+++ Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggerCookBook.Models
+{
+    public abstract class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Instructions { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
