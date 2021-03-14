9b33ac6a067471c0723d3e0902444113abd9b7a8
Isaac Heist <isaacheist@hotmail.com>
2021-03-14 16:54:13 -0700

    Remove unecessary initialize data.

--- Program.cs
+++ Program.cs
@@ -31,11 +31,6 @@ namespace BloggerCookBook
             if (addData)
             {
                 db.AddUser(new User { Username = "test", Password = "test", CreatedBy = "tester", CreatedDate = DateTime.Now });
-                db.AddIngredient(new Ingredient { Title = "Flour", MeasureType = "Volume", CreatedBy = "test", CreatedDate = DateTime.Now });
-                db.AddIngredient(new Ingredient { Title = "Baking Powder", MeasureType = "Volume", CreatedBy = "test", CreatedDate = DateTime.Now });
-                db.AddIngredient(new Ingredient { Title = "Salt", MeasureType = "Volume", CreatedBy = "test", CreatedDate = DateTime.Now });
-                db.AddIngredient(new Ingredient { Title = "Butter", MeasureType = "Volume", CreatedBy = "test", CreatedDate = DateTime.Now });
-                db.AddIngredient(new Ingredient {  Title = "Milk", MeasureType = "Volume", CreatedBy = "test", CreatedDate = DateTime.Now });
             }
             db.Close();
         }
