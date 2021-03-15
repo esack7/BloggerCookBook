280e310e763feb82bac2a92e5a0f913a0c8552b0
Isaac Heist <isaacheist@hotmail.com>
2021-03-14 20:15:06 -0700

    Adding Testing Solution

---
+++ BloggerCookBookTests/ControllerTests/DateFormatterUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using BloggerCookBook.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BloggerCookBookTests.ControllerTests
+{
+    [TestClass]
+    public class DateFormatterUnitTest
+    {
+        [TestMethod]
+        public void DateFormatterReturnsCorrectDateWhenDayBeginningCalled()
+        {
+            //Arrange
+            var date = new DateTime(2021, 3, 14, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            //Act
+            var newDateFormatter = new DateFormatter(date);
+            var actual = newDateFormatter.DayBeginning();
+            var expected = new DateTime(2021, 3, 14);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void DateFormatterReturnsCorrectToStringMethod()
+        {
+            //Arrange
+            var date = new DateTime(2021, 3, 14, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            //Act
+            var newDateFormatter = new DateFormatter(date);
+            var actual = newDateFormatter.ToString();
+            var expected = "3-14-2021";
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ BloggerCookBookTests/ModelTests/RecipeUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using BloggerCookBook.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace BloggerCookBookUnitTests
+{
+    [TestClass]
+    public class RecipeUnitTest
+    {
+        [TestMethod]
+        public void WebRecipeIsOfTypeRecipeTest()
+        {
+            //Arrange
+            WebRecipe actual = new WebRecipe();
+            List<Recipe> listofRecipes = new List<Recipe>();
+            //Act
+            listofRecipes.Add(actual);
+            var expected = listofRecipes[0];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PersonalRecipeIsOfTypeRecipeTest()
+        {
+            //Arrange
+            PersonalRecipe actual = new PersonalRecipe();
+            List<Recipe> listofRecipes = new List<Recipe>();
+            //Act
+            listofRecipes.Add(actual);
+            var expected = listofRecipes[0];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BookRecipeIsOfTypeRecipeTest()
+        {
+            //Arrange
+            BookRecipe actual = new BookRecipe();
+            List<Recipe> listofRecipes = new List<Recipe>();
+            //Act
+            listofRecipes.Add(actual);
+            var expected = listofRecipes[0];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
